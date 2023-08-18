8a7bbd096fbb174d1fbf79cbdc33c24ccf92a596
Leslie <Kooliesk@gmail.com>
2023-08-18 19:45:59 +0200

    and reserve form component and route

---
+++ src/components/forms/reserveForm.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MyReserveForm = () => (
+  <div>reserveForm</div>
+);
+
+export default MyReserveForm;
---
+++ src/routes/ReserveForm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MyReserveForm from '../components/forms/addReservation';
+
+const ReserveForm = () => (
+  <div>
+    <MyReserveForm />
+  </div>
+);
+
+export default ReserveForm;
