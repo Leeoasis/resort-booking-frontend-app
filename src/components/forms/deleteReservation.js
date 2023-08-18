24554adf71e3fab4a9b590a1785ed3f6142686ad
Leslie <Kooliesk@gmail.com>
2023-08-18 19:45:21 +0200

    add delete reservation component and route

---
+++ src/components/forms/deleteReservation.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MyDeleteReservation = () => (
+  <div>Delete Reservation</div>
+);
+
+export default MyDeleteReservation;
---
+++ src/routes/DeleteReservation.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MyDeleteReservation from '../components/forms/deleteReservation';
+
+const DeleteReservation = () => (
+  <div>
+    <MyDeleteReservation />
+  </div>
+);
+
+export default DeleteReservation;
