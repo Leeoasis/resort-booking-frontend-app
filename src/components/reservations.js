ddb496d720dbb0ffa181c4642888e642db838660
Leslie <Kooliesk@gmail.com>
2023-08-18 19:44:14 +0200

    add reservertions component and route

---
+++ src/components/reservations.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MyReservations = () => (
+  <div>My Reservations</div>
+);
+
+export default MyReservations;
---
+++ src/routes/Reservations.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MyReservations from '../components/reservations';
+
+const Reservations = () => (
+  <div>
+    <MyReservations />
+  </div>
+);
+
+export default Reservations;
