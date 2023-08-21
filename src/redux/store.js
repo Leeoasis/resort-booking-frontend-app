856b8ba3dc5ce283e363156aa534b50ace467831
Lawrence Amoafo <rekenburg777@gmail.com>
2023-08-21 19:15:37 +0000

    Add reducer to redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
+import resortReducers from './features/resortSlice';
 
 const store = configureStore({
   reducer: {
-
+    resort: resortReducers,
   },
 });
 
