b75e60334fb6806fab79188290db54a24d1a64df
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-08-25 14:59:58 +0530

    Add latest changes

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -20,7 +20,7 @@ function Home() {
         desktop: {
           breakpoint: { max: 3000, min: 1024 },
           items: 3,
-          slidesToSlide: 3 // optional, default to 1.
+          slidesToSlide: 1 // optional, default to 1.
         },
         tablet: {
           breakpoint: { max: 1024, min: 464 },
